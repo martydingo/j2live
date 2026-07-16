67ff00883c5250fb6ccf183727f66b3dc81a6f1a
Martin George <marty@dingo.foo>
2026-07-16 11:28:12 +1000

    Removed prints

--- j2live/ansible/__init__.py
+++ j2live/ansible/__init__.py
@@ -8,9 +8,6 @@ def renderTemplate(yamlVars, jinjaTemplate: str):
     loader = DataLoader()
     variables = yaml.safe_load(yamlVars) or {}
 
-    print(f"yamlVars: {variables}")
-    print(f"jinjaTemplate: {jinjaTemplate}")
-
     templar = Templar(loader=loader, variables=variables)
     trusted_template = trust_as_template(jinjaTemplate)
 
